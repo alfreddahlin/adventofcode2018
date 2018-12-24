29dee7904951e48f9137569b182101c8654ef4a8
Alfred Dahlin <daahlin@hotmail.com>
2018-12-24 14:46:01 +0100

    day24

---
+++ day23.py
@@ -0,0 +1,52 @@
+from itertools import product
+import re
+
+input_data = open('day23.in','r').read().strip().split('\n')
+
+data = [re.findall(r'-?\d+',line) for line in input_data]
+bots = {(int(x),int(y),int(z)): int(r) for x,y,z,r in data}
+
+
+def get_distance(pos,target):
+    return sum(abs(c1-c2) for c1,c2 in zip(pos,target))
+
+def get_corners(bot,r):
+    return {(bot[0]+x,bot[1]+y,bot[2]+z): bot for x,y,z in [(-r,0,0),(r,0,0),(0,-r,0),(0,r,0),(0,0,-r),(0,0,r)]}
+
+r_max = ((0,0,0),0)
+for bot,r in bots.items():
+    if r > r_max[1]:
+        in_range = sum(get_distance(bot,b) <= r for b in bots)
+        r_max = ((bot),r)
+
+print('Part 1:',in_range)
+bots_corners = {}
+{bots_corners.update(get_corners(bot,r)) for bot,r in bots.items()}
+
+r_max = ((0,0,0),0)
+for pos in bots_corners:
+    n = sum(get_distance(pos,bot)<=r for bot,r in bots.items())
+    if n>=r_max[1]:
+        if n == r_max[1] and sum(r_max[0]) < sum(pos):
+            continue
+        r_max = (pos,n)
+bot_max = bots_corners[r_max[0]]
+corner_max = r_max[0]
+
+r_final = []
+for dx,dy,dz in [(dx,dy,dz) for dx,dy,dz in product([-1,0,1],repeat=3) if dx**2+dy**2+dz**2 == 2]:
+    pos = corner_max
+    r_max = ((0,0,0),0)
+    n = r_max[1]
+    while n >= r_max[1]:
+        n = sum(get_distance(pos,bot)<=r for bot,r in bots.items())
+        if n>=r_max[1]:
+            if n == r_max[1] and sum(r_max[0]) <= sum(pos):
+                pass
+            else:
+                r_max = (pos,n)
+        pos = (pos[0]+dx,pos[1]+dy,pos[2]+dz)
+    r_final.append(r_max)
+print(r_final)
+max_r = max(r_final,key = lambda k: (k[1],sum(k[0])))
+print('Part 2:',sum(r_max[0]))
---
+++ day24.py
@@ -0,0 +1,70 @@
+import re
+import copy
+
+input_data = [section.split('\n') for section in open('day24.in','r').read().strip().split('\n\n')]
+
+data = [[re.findall(r'(\d+)[\s\S]+?(\d+)[\s\S]+?\(([\s\S]+?)?\)[\s\S]+?(\d+) ([a-z]+)[\s\S]+?(\d+)',group) for group in army] for army in input_data]
+
+groups_initial = {}
+groups_initial.update({int(group[0][-1]): {'units': int(group[0][0]),'HP': int(group[0][1]),'immune': set(),'weak': set(),'damage': int(group[0][-3]),'attack': group[0][-2],'army': 'immune'} for group in data[0] if group})
+groups_initial.update({int(group[0][-1]): {'units': int(group[0][0]),'HP': int(group[0][1]),'immune': set(),'weak': set(),'damage': int(group[0][-3]),'attack': group[0][-2],'army': 'infect'} for group in data[1] if group})
+
+data = [group[0] for army in data for group in army if group]
+
+[groups_initial[int(group[-1])][mod.split(' ')[0]].add(word.strip(',')) for group in data for mod in group[2].split('; ') for word in mod.split(' ')[2:]]
+
+def get_power(group):
+    return group['units']*group['damage']
+
+def get_damage(attacker,defender):
+    attack = attacker['attack']
+    modifier = (attack not in defender['immune']) * (1 + (attack in defender['weak']))
+    return get_power(attacker)*modifier
+
+def attack(attacker,defender):
+    defender['units'] -= get_damage(attacker,defender)//defender['HP']
+    if defender['units'] < 1:
+        return {}
+    else:
+        return defender
+
+boost_range = [-2000,2000]
+boost_units = 0
+remaining = 'infect'
+while boost_range[0] != boost_range[1]:
+    boost = sum(boost_range)//2
+    groups = copy.deepcopy(groups_initial)
+    {groups[i].update({'damage': groups_initial[i]['damage']+boost}) for i in groups if groups[i]['army'] == 'immune'}
+    new_units = sum(group['units'] for group in groups.values())
+    while not all(v['army']=='immune' for v in groups.values()) and not all(v['army'] == 'infect' for v in groups.values()):
+
+        # Targeting
+        targets = {}
+        remaining = set(groups.keys())
+        units = new_units
+        while remaining:
+            selecting = max(remaining,key = lambda k: (get_power(groups[k]),k))
+            possible = {group for group,props in groups.items() if group not in targets.values() and groups[selecting]['army'] != props['army'] and get_damage(groups[selecting],props)}
+            if possible:
+                targets[selecting] = max(possible,key = lambda k: (get_damage(groups[selecting],groups[k]),get_power(groups[k]),k))
+            remaining.remove(selecting)
+
+        # Attacking
+        while targets:
+            attacking = max(targets)
+            defending = targets.pop(attacking)
+            if attacking in groups:
+                defender = attack(groups[attacking],groups.pop(defending))
+                if defender:
+                    groups[defending] = defender
+        new_units = sum(group['units'] for group in groups.values())
+        if units == new_units:
+            break
+    if boost == 0:
+        print('Part 1:',new_units)
+    if groups[list(groups.keys())[-1]]['army'] == 'infect' or units == new_units:
+        boost_range[0] = boost+1
+    else:
+        boost_range[1] = boost
+        boost_units = new_units
+print('Part 2:',boost_units)
